9f0292a2ed4aedec039610d64f3fc090a1a3f734
DeepthiGirijaThampi <deepthig01@gmail.com>
2026-06-10 17:15:20 -0500

    Add ThreeSumClosest class

---
+++ src/main/java/com/deepthi/interviewprep/ThreeSumClosest.java
@@ -0,0 +1,18 @@
+package com.deepthi.interviewprep;
+
+import java.util.Arrays;
+//Given an integer array nums of length n and an integer target, find three integers at distinct indices in nums such that the sum is closest to target.
+//
+//Return the sum of the three integers.
+//
+//You may assume that each input would have exactly one solution.
+
+public class ThreeSumClosest {
+
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+
+        return 1;
+    }
+
+}

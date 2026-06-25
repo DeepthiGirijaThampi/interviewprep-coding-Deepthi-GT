c2132f3021230bd757cb36bd576e6ca8e8c01a0b
DeepthiGirijaThampi <deepthig01@gmail.com>
2026-06-25 09:40:53 -0500

    Add Node class

---
+++ src/main/java/com/deepthi/interviewprep/Node.java
@@ -0,0 +1,12 @@
+package com.deepthi.interviewprep;
+
+public class Node {
+
+    int data;// stores the value
+    Node next;// reference to next node
+
+    public Node(int data) {
+        this.data = data; //assined a value
+        this.next = null; // initially no next node
+    }
+}
